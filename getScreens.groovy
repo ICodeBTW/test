51b337fa547544502e9ca9fc5677671c48f8b513
Shreenidhi Bharadwaj H K <92727509+ICodeBTW@users.noreply.github.com>
2023-02-27 19:39:04 +0530

    Update getScreens.groovy

--- getScreens.groovy
+++ getScreens.groovy
@@ -1,18 +1,50 @@
 import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.fields.CustomField
+import com.atlassian.jira.issue.fields.layout.field.FieldLayout
+import com.atlassian.jira.issue.fields.screen.FieldScreen
+import com.atlassian.jira.issue.fields.screen.FieldScreenTab
+import com.atlassian.jira.project.Project
+import com.atlassian.jira.project.ProjectManager
 
-def customFieldManager = ComponentAccessor.getCustomFieldManager()
-def customField = customFieldManager.getCustomFieldObjectByName("Custom Field Name")
+// Get the custom field by ID
+CustomField customField = ComponentAccessor.getCustomFieldManager().getCustomFieldObject("customfield_12345")
 
-def fieldLayoutManager = ComponentAccessor.getFieldLayoutManager()
-def fieldLayouts = fieldLayoutManager.getFieldLayoutsForCustomField(customField)
+// Initialize an empty list to hold screen IDs that include the custom field
+List<Long> screensWithField = []
 
-def screens = []
-for (fieldLayout in fieldLayouts) {
-    screens.addAll(fieldLayout.getAssociatedScreens())
+// Get the project manager
+ProjectManager projectManager = ComponentAccessor.getProjectManager()
+
+// Iterate through all projects
+for (Project project : projectManager.getProjects()) {
+    
+    // Get the field layout for the custom field in the project
+    FieldLayout fieldLayout = ComponentAccessor.getFieldLayoutManager().getFieldLayout(project, customField)
+    
+    // Check if the field layout contains the custom field
+    if (fieldLayout.contains(customField)) {
+        
+        // Get the field screen for the field layout
+        FieldScreen fieldScreen = fieldLayout.getFieldScreen()
+        
+        // Iterate through all tabs of the field screen
+        for (FieldScreenTab tab : fieldScreen.getTabs()) {
+            
+            // Check if the tab contains the custom field
+            if (tab.getFieldScreenLayoutItem(customField.getId()) != null) {
+                
+                // Add the screen ID to the list
+                screensWithField.add(fieldScreen.getId())
+                break
+            }
+        }
+    }
 }
 
-screens = screens.unique()
-log.debug("Screens containing {}:", customField.name)
-for (screen in screens) {
-    log.debug(" - {}", screen.name)
+// Remove duplicate screen IDs
+screensWithField = screensWithField.unique()
+
+// Print the screen IDs
+for (Long screenId : screensWithField) {
+    log.debug("Screen ID: {}", screenId)
 }
