fe437021a9de5fce764155d46ca17206a077022e
Shreenidhi Bharadwaj H K <92727509+ICodeBTW@users.noreply.github.com>
2023-03-03 01:04:05 +0530

    Update getScreens.groovy

--- getScreens.groovy
+++ getScreens.groovy
@@ -1,79 +1,68 @@
-<!DOCTYPE html>
-<meta charset="utf-8">
-<style>
+function update(source) {
+  // Compute the new tree layout.
+  const updatedData = treeLayout(root);
 
-.node circle {
-  fill: #999;
+  // Update the links.
+  const link = g.selectAll("path.link")
+    .data(updatedData.links());
+  
+  link.enter()
+    .append("path")
+    .attr("class", "link")
+    .merge(link)
+    .transition()
+    .duration(duration)
+    .attr("d", link);
+  
+  link.exit().remove();
+
+  // Update the nodes.
+  const node = g.selectAll("g.node")
+    .data(updatedData.descendants(), d => d.id);
+  
+  const nodeEnter = node.enter()
+    .append("g")
+    .attr("class", "node")
+    .attr("transform", d => `rotate(${source.x0 * 180 / Math.PI - 90}) translate(${source.y0},0)`)
+    .on("click", d => {
+      d.children = d.children ? null : d._children;
+      update(d);
+    });
+  
+  nodeEnter.append("circle")
+    .attr("r", 1e-6)
+    .style("fill", d => d._children ? "#ccc" : "#fff");
+
+  nodeEnter.append("text")
+    .attr("dy", "0.31em")
+    .attr("x", d => d.x < Math.PI === !d.children ? 6 : -6)
+    .attr("text-anchor", d => d.x < Math.PI === !d.children ? "start" : "end")
+    .attr("transform", d => d.x < Math.PI ? null : "rotate(180)")
+    .text(d => d.data.name)
+    .clone(true)
+    .lower()
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-width", 3)
+    .attr("stroke", "white");
+  
+  const nodeUpdate = nodeEnter.merge(node);
+  
+  nodeUpdate.transition()
+    .duration(duration)
+    .attr("transform", d => `rotate(${d.x * 180 / Math.PI - 90}) translate(${d.y},0)`);
+  
+  nodeUpdate.select("circle")
+    .attr("r", 4.5)
+    .style("fill", d => d._children ? "#ccc" : "#fff");
+  
+  node.exit().transition()
+    .duration(duration)
+    .attr("transform", d => `rotate(${source.x * 180 / Math.PI - 90}) translate(${source.y},0)`)
+    .remove();
+  
+  // Stash the old positions for transition.
+  updatedData.each(d => {
+    d.x0 = d.x;
+    d.y0 = d.y;
+  });
 }
-
-.node text {
-  font: 10px sans-serif;
-}
-
-.node--internal circle {
-  fill: #555;
-}
-
-.node--internal text {
-  text-shadow: 0 1px 0 #fff, 0 -1px 0 #fff, 1px 0 0 #fff, -1px 0 0 #fff;
-}
-
-.link {
-  fill: none;
-  stroke: #555;
-  stroke-opacity: 0.4;
-  stroke-width: 1.5px;
-}
-
-</style>
-<svg width="960" height="1060"></svg>
-<script src="https://d3js.org/d3.v4.min.js"></script>
-<script>
-
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height"),
-    g = svg.append("g").attr("transform", "translate(" + (width / 2 + 40) + "," + (height / 2 + 90) + ")");
-
-var stratify = d3.stratify()
-    .parentId(function(d) { return d.id.substring(0, d.id.lastIndexOf(".")); });
-
-var tree = d3.tree()
-    .size([2 * Math.PI, 500])
-    .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
-
-d3.csv("flare.csv", function(error, data) {
-  if (error) throw error;
-
-  var root = tree(stratify(data));
-
-  var link = g.selectAll(".link")
-    .data(root.links())
-    .enter().append("path")
-      .attr("class", "link")
-      .attr("d", d3.linkRadial()
-          .angle(function(d) { return d.x; })
-          .radius(function(d) { return d.y; }));
-
-  var node = g.selectAll(".node")
-    .data(root.descendants())
-    .enter().append("g")
-      .attr("class", function(d) { return "node" + (d.children ? " node--internal" : " node--leaf"); })
-      .attr("transform", function(d) { return "translate(" + radialPoint(d.x, d.y) + ")"; });
-
-  node.append("circle")
-      .attr("r", 2.5);
-
-  node.append("text")
-      .attr("dy", "0.31em")
-      .attr("x", function(d) { return d.x < Math.PI === !d.children ? 6 : -6; })
-      .attr("text-anchor", function(d) { return d.x < Math.PI === !d.children ? "start" : "end"; })
-      .attr("transform", function(d) { return "rotate(" + (d.x < Math.PI ? d.x - Math.PI / 2 : d.x + Math.PI / 2) * 180 / Math.PI + ")"; })
-      .text(function(d) { return d.id.substring(d.id.lastIndexOf(".") + 1); });
-});
-
-function radialPoint(x, y) {
-  return [(y = +y) * Math.cos(x -= Math.PI / 2), y * Math.sin(x)];
-}
-
-</script>
