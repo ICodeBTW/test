8887f0ae852cd7d58eaec4372f8e6b4627e1b6df
Shreenidhi Bharadwaj H K <92727509+ICodeBTW@users.noreply.github.com>
2023-03-02 14:50:40 +0530

    Update getScreens.groovy

--- getScreens.groovy
+++ getScreens.groovy
@@ -1,4 +1,4 @@
-// Set the dimensions and margins of the diagram
+ // Set the dimensions and margins of the diagram
 var margin = {top: 20, right: 90, bottom: 30, left: 90},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -9,35 +9,6 @@ var svg = d3.select("body").append("svg")
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + (margin.left + width / 2) + "," + (margin.top + height / 2) + ")");
-
-// Create a hierarchical data structure
-var treeData = {
-  "name": "A",
-  "children": [
-    {
-      "name": "B",
-      "children": [
-        {
-          "name": "C"
-        },
-        {
-          "name": "D"
-        }
-      ]
-    },
-    {
-      "name": "E",
-      "children": [
-        {
-          "name": "F"
-        },
-        {
-          "name": "G"
-        }
-      ]
-    }
-  ]
-};
 
 // Create a tree layout and set the size of the tree
 var treeLayout = d3.tree()
@@ -60,19 +31,16 @@ svg.selectAll(".link")
     .data(treeLinks)
     .enter().append("path")
     .attr("class", "link")
-    .attr("d", function(d) {
-      return "M" + project(d.x, d.y)
-           + "C" + project(d.x, (d.y + d.parent.y) / 2)
-           + " " + project(d.parent.x, (d.y + d.parent.y) / 2)
-           + " " + project(d.parent.x, d.parent.y);
-    });
+    .attr("d", d3.linkRadial()
+        .angle(function(d) { return d.x / 180 * Math.PI; })
+        .radius(function(d) { return d.y; }));
 
 // Draw the nodes
 var node = svg.selectAll(".node")
     .data(treeNodes)
     .enter().append("g")
     .attr("class", "node")
-    .attr("transform", function(d) { return "translate(" + project(d.x, d.y) + ")"; });
+    .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; });
 
 node.append("circle")
     .attr("r", 10);
@@ -81,5 +49,4 @@ node.append("text")
     .attr("dy", ".31em")
     .attr("x", function(d) { return d.x < 180 === !d.children ? 6 : -6; })
     .style("text-anchor", function(d) { return d.x < 180 === !d.children ? "start" : "end"; })
-    .attr("transform", function(d) { return "rotate(" + (d.x < 180 ? d.x - 90 : d.x + 90) + ")"; })
-    .
+    .attr("transform", function(d) { return "rotate(" + (d.x < 180 ? d.x - 90 : d.x + 90) + ")"; });
