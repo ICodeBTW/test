c94d054718d0c74a69fd074c368d6988dbf2da63
Shreenidhi Bharadwaj H K <92727509+ICodeBTW@users.noreply.github.com>
2023-03-03 16:03:27 +0530

    Update getScreens.groovy

--- getScreens.groovy
+++ getScreens.groovy
@@ -1,17 +1,8 @@
-function diagonal(d) {
-  const sourceX = d.source.x;
-  const sourceY = d.source.y;
-  const targetX = d.target.x;
-  const targetY = d.target.y;
-
-  const r = Math.sqrt(sourceX * sourceX + sourceY * sourceY);
-  const radialSource = [sourceY, sourceX / r * 180 / Math.PI];
-  const radialTarget = [targetY, targetX / r * 180 / Math.PI];
-
-  const diagonalPath = d3.linkRadial()
-    .angle((d) => d[1])
-    .radius((d) => d[0])
-    ({"source": radialSource, "target": radialTarget});
-
-  return diagonalPath;
-}
+var zoom = d3.behavior.zoom()
+    .scaleExtent([0.5, 10]) // set the minimum and maximum zoom scale
+    .on("zoom", function () {
+        // get the current zoom transform
+        var transform = d3.event.translate + " " + d3.event.scale;
+        // apply the zoom transform to the SVG elements
+        svg.attr("transform", transform);
+    });
