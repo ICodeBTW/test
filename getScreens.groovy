83da99d9393e7e540348c5ec3ba0e4c09484901e
Shreenidhi Bharadwaj H K <92727509+ICodeBTW@users.noreply.github.com>
2023-03-03 14:52:50 +0530

    Update getScreens.groovy

--- getScreens.groovy
+++ getScreens.groovy
@@ -1,68 +1,109 @@
+const treeLayout = tree()
+    .size([2 * Math.PI, radius])
+    .separation((a, b) => (a.parent == b.parent ? 1 : 2) / a.depth);
+const treeLayout = tree()
+    .size([2 * Math.PI, radius])
+    .separation((a, b) => (a.parent == b.parent ? 1 : 2) / a.depth);
+const root = hierarchy(data);
+root.x0 = Math.PI / 2;
+root.y0 = 0;
 function update(source) {
-  // Compute the new tree layout.
-  const updatedData = treeLayout(root);
+    const nodes = root.descendants().reverse();
+    const links = root.links();
 
-  // Update the links.
-  const link = g.selectAll("path.link")
-    .data(updatedData.links());
-  
-  link.enter()
-    .append("path")
-    .attr("class", "link")
-    .merge(link)
-    .transition()
-    .duration(duration)
-    .attr("d", link);
-  
-  link.exit().remove();
+    // Assigns the x and y position for the nodes
+    const treeData = treeLayout(root);
 
-  // Update the nodes.
-  const node = g.selectAll("g.node")
-    .data(updatedData.descendants(), d => d.id);
-  
-  const nodeEnter = node.enter()
-    .append("g")
-    .attr("class", "node")
-    .attr("transform", d => `rotate(${source.x0 * 180 / Math.PI - 90}) translate(${source.y0},0)`)
-    .on("click", d => {
-      d.children = d.children ? null : d._children;
-      update(d);
-    });
-  
-  nodeEnter.append("circle")
-    .attr("r", 1e-6)
-    .style("fill", d => d._children ? "#ccc" : "#fff");
+    // Normalize for fixed-depth
+    nodes.forEach((d) => (d.y = d.depth * 80));
 
-  nodeEnter.append("text")
-    .attr("dy", "0.31em")
-    .attr("x", d => d.x < Math.PI === !d.children ? 6 : -6)
-    .attr("text-anchor", d => d.x < Math.PI === !d.children ? "start" : "end")
-    .attr("transform", d => d.x < Math.PI ? null : "rotate(180)")
-    .text(d => d.data.name)
-    .clone(true)
-    .lower()
-    .attr("stroke-linejoin", "round")
-    .attr("stroke-width", 3)
-    .attr("stroke", "white");
-  
-  const nodeUpdate = nodeEnter.merge(node);
-  
-  nodeUpdate.transition()
-    .duration(duration)
-    .attr("transform", d => `rotate(${d.x * 180 / Math.PI - 90}) translate(${d.y},0)`);
-  
-  nodeUpdate.select("circle")
-    .attr("r", 4.5)
-    .style("fill", d => d._children ? "#ccc" : "#fff");
-  
-  node.exit().transition()
-    .duration(duration)
-    .attr("transform", d => `rotate(${source.x * 180 / Math.PI - 90}) translate(${source.y},0)`)
-    .remove();
-  
-  // Stash the old positions for transition.
-  updatedData.each(d => {
-    d.x0 = d.x;
-    d.y0 = d.y;
-  });
+    // ****************** Nodes section ***************************
+
+    // Update the nodes...
+    const node = svg.selectAll('g.node').data(nodes, (d) => d.id);
+
+    // Enter any new nodes at the parent's previous position
+    const nodeEnter = node
+        .enter()
+        .append('g')
+        .attr('class', 'node')
+        .attr('transform', (d) => `translate(${source.x0},${source.y0})`)
+        .on('click', (event, d) => {
+            toggle(d);
+            update(d);
+        });
+
+    nodeEnter
+        .append('circle')
+        .attr('class', 'node')
+        .attr('r', 10)
+        .style('fill', (d) => (d._children ? 'lightsteelblue' : '#fff'));
+
+    nodeEnter
+        .append('text')
+        .attr('dy', '.35em')
+        .attr('x', (d) => (d.children || d._children ? -13 : 13))
+        .attr('text-anchor', (d) => (d.children || d._children ? 'end' : 'start'))
+        .text((d) => d.data.name);
+
+    // Update the nodes
+    const nodeUpdate = node.merge(nodeEnter).transition().duration(duration).attr('transform', (d) => `translate(${d.x},${d.y})`);
+
+    nodeUpdate.select('circle.node').attr('r', 10).style('fill', (d) => (d._children ? 'lightsteelblue' : '#fff'));
+
+    // Transition exiting nodes to the parent's new position.
+    const nodeExit = node.exit().transition().duration(duration).attr('transform', (d) => `translate(${source.x},${source.y})`).remove();
+
+    nodeExit.select('circle').attr('r', 1e-6);
+
+    nodeExit.select('text').style('fill-opacity', 1e-6);
+
+    // ****************** links section ***************************
+
+    // Update the links...
+    const link = svg.selectAll('path.link').data(links, (d) => d.target.id);
+
+    // Enter any new links at the parent's previous position.
+    const linkEnter = link
+        .enter()
+        .insert('path', 'g')
+        .attr('class', 'link')
+       
+        .attr('d', (d) => {
+            const o = { x: source.x0, y: source.y0 };
+            return diagonal({ source: o, target: o });
+        });
+
+    // Update the links.
+    const linkUpdate = link.merge(linkEnter).transition().duration(duration).attr('d', diagonal);
+
+    // Transition exiting nodes to the parent's new position.
+    link.exit().transition().duration(duration).attr('d', (d) => {
+        const o = { x: source.x, y: source.y };
+        return diagonal({ source: o, target: o });
+    }).remove();
+
+    // ****************** Toggle section ***************************
+
+    // Toggle children on click.
+    function toggle(d) {
+        if (d.children) {
+            d._children = d.children;
+            d.children = null;
+        } else {
+            d.children = d._children;
+            d._children = null;
+        }
+    }
+
+    // ****************** Zoom section ***************************
+
+    function zoomed({ transform }) {
+        svg.attr('transform', transform);
+    }
+
+    const zoomBehavior = zoom().on('zoom', zoomed);
+
+    svg.call(zoomBehavior);
 }
+update(root);
