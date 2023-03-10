35ed7499a281d6b90cf517305bc1e6b65597f766
Shreenidhi Bharadwaj H K <92727509+ICodeBTW@users.noreply.github.com>
2023-03-10 13:29:25 +0530

    Update getScreens.groovy

--- getScreens.groovy
+++ getScreens.groovy
@@ -1,19 +1,15 @@
-import com.atlassian.jira.component.ComponentAccessor
-import com.atlassian.jira.issue.fields.CustomField
-import com.atlassian.jira.issue.fields.layout.field.FieldLayout
-import com.atlassian.jira.issue.fields.layout.field.FieldLayoutManager
-
-def fieldManager = ComponentAccessor.getFieldManager()
-def customField = fieldManager.getCustomFieldObject("customfield_12345") // Replace with your custom field ID
-def fieldLayoutManager = ComponentAccessor.getFieldLayoutManager()
-def screens = fieldLayoutManager.getEditableScreens()
-
-screens.each { screen ->
-    if (screen.id == "10001") { // Replace with your screen ID
-        def fieldLayout = fieldLayoutManager.getFieldLayout(screen.id, customField)
-        if (!fieldLayout) {
-            fieldLayout = fieldLayoutManager.addCustomFieldToScreen(customField, screen.id)
-            log.debug("Enabled custom field ${customField.name} in screen ${screen.name}")
+screenIds.each { screenId ->
+    def fieldScreen = fieldScreenManager.getFieldScreen(screenId)
+    if (fieldScreen) {
+        def fieldScreenTab = fieldScreen.getTab(0)
+        if (fieldScreenTab) {
+            def fieldScreenLayoutItem = fieldScreenTab.getFieldScreenLayoutItem(fieldId)
+            if (fieldScreenLayoutItem == null) {
+                def fieldLayoutItem = fieldManager.getFieldLayout(screenId).getFieldLayoutItem(fieldId)
+                if (fieldLayoutItem != null) {
+                    fieldScreenTab.addLayoutItem(fieldLayoutItem)
+                }
+            }
         }
     }
 }
