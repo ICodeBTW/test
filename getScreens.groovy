a75b7f8b0977bd42a90832685e2e511a4f8010c7
Shreenidhi Bharadwaj H K <92727509+ICodeBTW@users.noreply.github.com>
2023-03-10 16:35:49 +0530

    Update getScreens.groovy

--- getScreens.groovy
+++ getScreens.groovy
@@ -1,14 +1,19 @@
-screenIds.each { screenId ->
-    def fieldScreen = fieldScreenManager.getFieldScreen(screenId)
-    if (fieldScreen) {
-        fieldScreen.getTabs().each { tab ->
-            def fieldScreenLayoutItem = tab.getFieldScreenLayoutItem(fieldId)
-            if (fieldScreenLayoutItem == null) {
-                def fieldLayoutItem = fieldManager.getFieldLayout(screenId).getFieldLayoutItem(fieldId)
-                if (fieldLayoutItem != null) {
-                    tab.addLayoutItem(fieldLayoutItem)
-                }
-            }
+// Replace 'CUSTOM_FIELD_NAME' with the name of your custom field
+CustomField customField = ComponentAccessor.getCustomFieldManager().getCustomFieldObjectByName("CUSTOM_FIELD_NAME")
+
+// Replace the list of project IDs with your own list
+List<String> projectIDs = ["PROJECT_ID_1", "PROJECT_ID_2", "PROJECT_ID_3"]
+
+projectIDs.each { projectID ->
+    Project project = ComponentAccessor.getProjectManager().getProjectObjByKey(projectID)
+    if (project) {
+        if (!project.getCustomFieldObjects().contains(customField)) {
+            ComponentAccessor.getProjectManager().updateProject(project, project.getIssueTypes(), project.getAssigneeType(),
+                project.getProjectLead(), project.getUrl(), project.getDescription(), customField)
+        } else {
+            log.warn("Custom field {} already exists on project {}.", customField.name, project.key)
         }
+    } else {
+        log.warn("Project with ID {} not found.", projectID)
     }
 }
