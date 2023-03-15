9845c609b8c6e6e60739c426238c34088e67f253
Shreenidhi Bharadwaj H K <92727509+ICodeBTW@users.noreply.github.com>
2023-03-15 17:10:41 +0530

    Update getScreens.groovy

--- getScreens.groovy
+++ getScreens.groovy
@@ -1,10 +1,14 @@
 import com.atlassian.jira.component.ComponentAccessor
 import com.atlassian.jira.issue.fields.CustomFieldManager
+import com.atlassian.jira.issue.fields.config.FieldConfigScheme
+import com.atlassian.jira.issue.fields.config.manager.FieldConfigSchemeManager
+import com.atlassian.jira.issue.fields.layout.field.FieldLayoutManager
 import com.atlassian.jira.project.Project
-import com.atlassian.jira.project.ProjectFieldLayoutSchemeHelper
 
 def customFieldManager = ComponentAccessor.getCustomFieldManager()
 def projectManager = ComponentAccessor.getProjectManager()
+def fieldConfigSchemeManager = ComponentAccessor.getComponent(FieldConfigSchemeManager)
+def fieldLayoutManager = ComponentAccessor.getComponent(FieldLayoutManager)
 
 //Replace the custom field ID with your custom field's ID
 def customField = customFieldManager.getCustomFieldObject("customfield_10000")
@@ -12,12 +16,18 @@ def customField = customFieldManager.getCustomFieldObject("customfield_10000")
 //Replace the project key with the key of the project to which you want to add the custom field
 def project = projectManager.getProjectByCurrentKey("PROJECT_KEY")
 
-//Add the custom field to the project's field configuration
-def fieldConfig = customField.getRelevantConfig(project)
-customFieldManager.createCustomFieldConfig(customField, [fieldConfig])
+//Get the project's field configuration scheme
+def fieldConfigScheme = fieldConfigSchemeManager.getConfigScheme(project)
 
-def projectFieldLayoutSchemeHelper = ComponentAccessor.getComponent(ProjectFieldLayoutSchemeHelper)
-def fieldLayoutScheme = projectFieldLayoutSchemeHelper.getEffectiveFieldLayoutScheme(project)
-def fieldLayout = fieldLayoutScheme.getFieldLayout(project.id)
-fieldLayout.addFieldLayoutItem(customField.id, 0, 0)
-projectFieldLayoutSchemeHelper.updateFieldLayoutScheme(fieldLayoutScheme, fieldLayout)
+//Create a new field configuration for the custom field
+def fieldConfig = customFieldManager.createFieldConfig(customField)
+
+//Add the custom field to the project's field configuration scheme
+fieldConfigScheme.getOneAndOnlyConfig().get().addFieldConfig(customField)
+
+//Update the project's field layout to include the custom field
+def fieldLayout = fieldLayoutManager.getFieldLayout(project)
+fieldLayoutManager.addCustomFieldToFieldLayout(fieldConfig, fieldLayout)
+
+//Associate the updated field configuration scheme with the project
+fieldConfigSchemeManager.updateFieldConfigScheme(fieldConfigScheme)
