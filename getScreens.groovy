f3fb7a70a2b1f9d4fabcf0eedc2ad4a730c36427
Shreenidhi Bharadwaj H K <92727509+ICodeBTW@users.noreply.github.com>
2023-03-16 13:52:53 +0530

    Update getScreens.groovy

--- getScreens.groovy
+++ getScreens.groovy
@@ -1,15 +1,17 @@
 import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.search.SearchProviderFactory
 
 def customFieldManager = ComponentAccessor.getCustomFieldManager()
 def sourceField = customFieldManager.getCustomFieldObjectByName("Source Field")
 def targetField = customFieldManager.getCustomFieldObjectByName("Target Field")
 
 def issueService = ComponentAccessor.getIssueService()
+def jqlSearchProvider = ComponentAccessor.getComponent(SearchProviderFactory).createSearchProvider(null)
 
-def query = issueService.newQueryBuilder().where().project("PROJECT_KEY").build()
-def results = issueService.query(currentUser, query, PagerFilter.getUnlimitedFilter())
+def query = jqlSearchProvider.getSearchQueryBuilder().project("PROJECT_KEY").buildQuery()
+def results = jqlSearchProvider.search(query, currentUser, new PagerFilter())
 
-results.each { issue ->
+results.issues.each { issue ->
     def sourceFieldValue = issue.getCustomFieldValue(sourceField)
     if (sourceFieldValue) {
         issue.setCustomFieldValue(targetField, sourceFieldValue)
