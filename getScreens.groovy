2e8930d1ee44812009554d499f496913491f77c0
Shreenidhi Bharadwaj H K <92727509+ICodeBTW@users.noreply.github.com>
2023-03-23 13:07:20 +0530

    Update getScreens.groovy

--- getScreens.groovy
+++ getScreens.groovy
@@ -1,15 +1,27 @@
-import com.onresolve.scriptrunner.runner.ScriptRunnerImpl
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.fields.CustomField
 
-def scriptRunner = ScriptRunnerImpl.getPlugin()
-def consoleAppender = scriptRunner.console.getAppender("Console")
+def customFieldManager = ComponentAccessor.getCustomFieldManager()
+def customField = customFieldManager.getCustomFieldObjectByName("Custom Field Name")
 
-// store the original value of MaxFileSize
-def originalMaxFileSize = consoleAppender.getMaxFileSize()
-
-// temporarily set MaxFileSize to a larger value
-consoleAppender.setMaxFileSize("10MB")
-
-// your script code here
-
-// reset MaxFileSize to its original value
-consoleAppender.setMaxFileSize(originalMaxFileSize)
+if (customField) {
+    customField.getConfigurationSchemes().each { scheme ->
+        scheme.getConfigurations().each { config ->
+            config.getConfigurationItem(customField.getId())?.setDisabled(true)
+        }
+        ComponentAccessor.getIssueTypeScreenSchemeManager().schemes.forEach { screenScheme ->
+            screenScheme.associatedSchemes.each { issueTypeScreenScheme ->
+                issueTypeScreenScheme.screenSchemes.each { screenSchemeItem ->
+                    screenSchemeItem.getTabs().each { tab ->
+                        tab.getFieldScreenRenderLayoutItems().each { layoutItem ->
+                            if (layoutItem.getOrderableField().getId() == customField.getId()) {
+                                layoutItem.setHidden(true)
+                                layoutItem.store()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
