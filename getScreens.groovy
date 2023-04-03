fc8b0d3cd130cfe405d20a555a7c51c2e8fc0068
Shreenidhi Bharadwaj H K <92727509+ICodeBTW@users.noreply.github.com>
2023-04-03 12:42:19 +0530

    Update getScreens.groovy

--- getScreens.groovy
+++ getScreens.groovy
@@ -1,15 +1,23 @@
 import com.atlassian.jira.component.ComponentAccessor
 import com.atlassian.jira.issue.fields.FieldManager
 import com.atlassian.jira.issue.fields.CustomField
-import com.atlassian.jira.config.IssueTypeScreenSchemeManager
+import com.atlassian.jira.project.Project
+import com.atlassian.jira.project.ProjectManager
+import com.atlassian.jira.issue.fields.layout.field.FieldLayout
+import com.atlassian.jira.issue.fields.layout.field.FieldLayoutItem
+import com.atlassian.jira.issue.fields.layout.field.FieldLayoutManager
 
 def fieldManager = ComponentAccessor.getFieldManager() as FieldManager
 def customField = fieldManager.getCustomFieldObjectByName("Name of Field to Disable")
-def issueTypeScreenSchemeManager = ComponentAccessor.getComponent(IssueTypeScreenSchemeManager)
+def projectKey = "PROJECT_KEY" // Replace with the key of the project where the field should be disabled
+def projectManager = ComponentAccessor.getComponent(ProjectManager)
+def project = projectManager.getProjectObjByKey(projectKey)
+def fieldLayoutManager = ComponentAccessor.getFieldLayoutManager()
 
-issueTypeScreenSchemeManager.getIssueTypeScreenSchemes().each { issueTypeScreenScheme ->
-    issueTypeScreenScheme.getIssueTypeMappings().each { issueType, screenScheme ->
-        screenScheme.removeFieldScreenLayoutItem(customField.id)
-        issueTypeScreenSchemeManager.updateIssueTypeScreenSchemeMappings(issueTypeScreenScheme, issueType, screenScheme)
-    }
+def fieldLayout = fieldLayoutManager.getFieldLayout(project)
+def fieldLayoutItem = fieldLayout.getFieldLayoutItem(customField)
+
+if (fieldLayoutItem) {
+    fieldLayout.removeFieldLayoutItem(fieldLayoutItem)
+    fieldLayoutManager.storeFieldLayout(fieldLayout)
 }
