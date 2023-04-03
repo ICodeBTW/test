9de5434c101ce6e788b66700843d01b6090741d8
Shreenidhi Bharadwaj H K <92727509+ICodeBTW@users.noreply.github.com>
2023-04-03 12:36:29 +0530

    Update getScreens.groovy

--- getScreens.groovy
+++ getScreens.groovy
@@ -1,27 +1,23 @@
 import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.fields.FieldManager
 import com.atlassian.jira.issue.fields.CustomField
 
-def customFieldManager = ComponentAccessor.getCustomFieldManager()
-def customField = customFieldManager.getCustomFieldObjectByName("Custom Field Name")
+def fieldManager = ComponentAccessor.getFieldManager() as FieldManager
+def customField = fieldManager.getCustomFieldObjectByName("Name of Field to Disable")
+def configurationSchemeManager = ComponentAccessor.getConfigurationSchemeManager()
 
-if (customField) {
-    customField.getConfigurationSchemes().each { scheme ->
-        scheme.getConfigurations().each { config ->
-            config.getConfigurationItem(customField.getId())?.setDisabled(true)
-        }
-        ComponentAccessor.getIssueTypeScreenSchemeManager().schemes.forEach { screenScheme ->
-            screenScheme.associatedSchemes.each { issueTypeScreenScheme ->
-                issueTypeScreenScheme.screenSchemes.each { screenSchemeItem ->
-                    screenSchemeItem.getTabs().each { tab ->
-                        tab.getFieldScreenRenderLayoutItems().each { layoutItem ->
-                            if (layoutItem.getOrderableField().getId() == customField.getId()) {
-                                layoutItem.setHidden(true)
-                                layoutItem.store()
-                            }
-                        }
-                    }
-                }
-            }
-        }
+configurationSchemeManager.getConfigurationSchemes().each { scheme ->
+    def config = configurationSchemeManager.getConfiguration(scheme, customField)
+    if (config) {
+        def fieldConfigScheme = configurationSchemeManager.getFieldConfigScheme(config)
+        def fieldConfig = fieldConfigScheme.getOneAndOnlyConfig()
+        fieldConfigScheme.removeConfiguration(fieldConfig)
+    }
+}
+
+customField.getConfigurationSchemes().each { scheme ->
+    def config = customField.getRelevantConfig(scheme)
+    if (config) {
+        customField.getConfigurationSchemes().removeSchemeAssociation(config.getSchemeId())
     }
 }
