a42b7db8f07200aea27982bb0a4e465578b62863
Shreenidhi Bharadwaj H K <92727509+ICodeBTW@users.noreply.github.com>
2023-04-19 14:24:15 +0530

    Update getScreens.groovy

--- getScreens.groovy
+++ getScreens.groovy
@@ -1,20 +1,30 @@
 import com.atlassian.jira.component.ComponentAccessor
 import com.atlassian.jira.issue.fields.CustomField
-import com.atlassian.jira.issue.fields.config.FieldConfigScheme
-// Get the custom field object for the source field
-CustomField sourceField = ComponentAccessor.getCustomFieldManager().getCustomFieldObjectByName("Source Custom Field Name")
-// Get the field configuration scheme for the source field
-FieldConfigScheme sourceScheme = sourceField.getConfigurationSchemes().listIterator().next()
-// Get the custom field object for the destination field
-CustomField destField = ComponentAccessor.getCustomFieldManager().getCustomFieldObjectByName("Destination Custom Field Name")
-// Create a new field configuration scheme for the destination field
-FieldConfigScheme destScheme = ComponentAccessor.getFieldConfigSchemeManager().createFieldConfigScheme(destField)
-// Copy the issue type to field configuration mappings from the source scheme to the destination scheme
-for (Object obj : sourceScheme.getConfigsByIssueType().entrySet()) {
-    Map.Entry entry = (Map.Entry) obj
-    def issueType = entry.key
-    def fieldConfig = entry.value
-    destScheme.getIssueTypeMappings().add(issueType, fieldConfig)
-}
-// Save the new field configuration scheme for the destination field
-ComponentAccessor.getFieldConfigSchemeManager().updateFieldConfigScheme(destScheme)
+import com.atlassian.jira.issue.fields.config.FieldConfig
+import com.atlassian.jira.issue.fields.config.manager.FieldConfigSchemeManager
+import com.atlassian.jira.issue.context.JiraContextNode
+
+def customFieldManager = ComponentAccessor.getCustomFieldManager()
+def fieldConfigSchemeManager = ComponentAccessor.getFieldConfigSchemeManager()
+
+// Specify the ID of the source custom field and the ID of the destination custom field
+def sourceCustomFieldId = 12345
+def destinationCustomFieldId = 67890
+
+// Get the FieldConfig object for the source custom field
+def sourceCustomField = customFieldManager.getCustomFieldObject(sourceCustomFieldId)
+def sourceFieldConfig = sourceCustomField.getRelevantConfig()
+
+// Get the FieldConfigScheme for the destination custom field
+def destinationCustomField = customFieldManager.getCustomFieldObject(destinationCustomFieldId)
+def destinationFieldConfigScheme = fieldConfigSchemeManager.getFieldConfigScheme(destinationCustomField)
+
+// Create a new FieldConfig based on the source custom field's configuration
+def destinationFieldConfig = fieldConfigSchemeManager.createWithDefaultValues(sourceFieldConfig, destinationCustomField.name)
+
+// Associate the new FieldConfig with the destination custom field
+def contexts = destinationFieldConfigScheme.getContexts()
+fieldConfigSchemeManager.updateFieldConfigScheme(destinationFieldConfigScheme, contexts, destinationCustomField)
+
+// Update the Field Configuration Scheme
+fieldConfigSchemeManager.updateFieldConfigScheme(destinationFieldConfigScheme, contexts, destinationCustomField)
